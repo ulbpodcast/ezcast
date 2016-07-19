c2c05830d2402ad4870b9642df7e5af1956bb8bc
Jan-David van Binsbergen <jan-david.van.binsbergen@ulb.ac.be>
2016-07-19 16:06:31 +0200

    EZadmin: Some more warnings

--- ezadmin/tmpl_sources/div_create_classroom.php
+++ ezadmin/tmpl_sources/div_create_classroom.php
@@ -13,28 +13,28 @@
     <div class="form-group">
         <label for="room_ID" class="col-sm-2 control-label">®classroom_id®</label>
         <div class="col-sm-5">
-            <input type="text" class="form-control" name="room_ID" value="<?php echo $input['room_ID']?>"/>
+            <input type="text" class="form-control" name="room_ID" value="<?php if(isset($input['room_ID'])) echo $input['room_ID']?>"/>
         </div>
     </div>
     
     <div class="form-group">
         <label for="name" class="col-sm-2 control-label">®classroom_name®</label>
         <div class="col-sm-5">
-            <input type="text" class="form-control" name="name" value="<?php echo $input['name']?>"/>
+            <input type="text" class="form-control" name="name" value="<?php if(isset($input['name'])) echo $input['name']?>"/>
         </div>
     </div>
     
     <div class="form-group">
         <label for="ip" class="col-sm-2 control-label">®classroom_ip®</label>
         <div class="col-sm-5">
-            <input type="text" class="form-control" name="ip" value="<?php echo $input['ip']?>"/>
+            <input type="text" class="form-control" name="ip" value="<?php if(isset($input['ip']))echo $input['ip']?>"/>
         </div>
     </div>
     
     <div class="form-group">
         <label for="ip_remote" class="col-sm-2 control-label">®classroom_remote_ip®</label>
         <div class="col-sm-5">
-            <input type="text" class="form-control" name="ip_remote" value="<?php echo $input['ip_remote']?>"/>
+            <input type="text" class="form-control" name="ip_remote" value="<?php if(isset($input['ip_remote']))echo $input['ip_remote']?>"/>
         </div>
     </div>
     
@@ -42,7 +42,7 @@
         <div class="col-sm-5 col-sm-offset-2">
             <div class="checkbox">
                 <label>
-                    <input type="checkbox" name="enabled" <?php echo $input['enabled'] ? 'checked' : ''?>/>®classroom_enabled®
+                    <input type="checkbox" name="enabled" <?php echo (isset($input['enabled']) && $input['enabled']) ? 'checked' : ''?>/>®classroom_enabled®
                 </label>
             </div>
         </div>
--- ezadmin/tmpl_sources/div_search_classroom.php
+++ ezadmin/tmpl_sources/div_search_classroom.php
@@ -8,9 +8,9 @@
     <input type="hidden" name="col" value="<?php echo $input['col'] ?>" />
     <input type="hidden" name="order" value="<?php echo $input['order'] ?>" />
     
-    <input class="form-control input-large auto-clear placeholder" type="text" placeholder="®room_ID®" title="®room_ID®" name="room_ID" value="<?php echo $input['room_ID']; ?>" />
-    <input class="form-control input-large auto-clear placeholder" type="text" placeholder="®room_name®" title="®room_name®" name="name" value="<?php echo $input['name']; ?>" />
-    <input class="form-control input-large auto-clear placeholder" type="text" placeholder="®room_IP®" title="®room_IP®" name="IP" value="<?php echo $input['IP']; ?>" />
+    <input class="form-control input-large auto-clear placeholder" type="text" placeholder="®room_ID®" title="®room_ID®" name="room_ID" value="<?php if(isset($input) && isset($input['room_ID'])) echo  $input['room_ID']; ?>" />
+    <input class="form-control input-large auto-clear placeholder" type="text" placeholder="®room_name®" title="®room_name®" name="name" value="<?php if(isset($input) && isset($input['name'])) echo $input['name']; ?>" />
+    <input class="form-control input-large auto-clear placeholder" type="text" placeholder="®room_IP®" title="®room_IP®" name="IP" value="<?php if(isset($input) && isset($input['IP'])) echo $input['IP']; ?>" />
 
     <input type="submit" name="search" value="®search®" class="btn btn-primary">
     <input type="reset" name="reset" value="®reset®" class="btn"> <br />

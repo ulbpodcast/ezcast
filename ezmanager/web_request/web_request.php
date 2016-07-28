fdb2c4ef1ebf42103816e66811e39aae49f6c7c9
Jan-David van Binsbergen <jan-david.van.binsbergen@ulb.ac.be>
2016-07-28 14:02:20 +0200

    Fixed broken manager web services ($caller_ip global variable was not global anymore)

--- ezmanager/web_request/web_request.php
+++ ezmanager/web_request/web_request.php
@@ -2,9 +2,13 @@
 
 require_once __DIR__."/../classroom_recorder_ip.inc"; //valid ip file
 
+//global variable
+$caller_ip = null;
+
 function isValidCaller() {
     global $podcv_ip;
     global $podcs_ip;
+    global $caller_ip;
     
     //look for caller's ip in config files
     $caller_ip = trim($_SERVER["REMOTE_ADDR"]);

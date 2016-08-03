192c900fd6a1644f5f3c8e32432f01514796da61
Rémy Detobel <detobel36@hotmail.fr>
2016-08-03 17:08:37 +0200

    [Commons] Add information in asset infos table when record begin and end

--- commons/logger_server.php
+++ commons/logger_server.php
@@ -8,9 +8,10 @@ class ServerLogger extends Logger {
     const EVENT_TABLE_NAME = "events";
     const EVENT_STATUS_TABLE_NAME = "event_status";    
     const EVENT_LAST_INDEXES_TABLE_NAME = "event_last_indexes";
-    const EVENT_ASSET_PARENT = "event_asset_parent";
+    const EVENT_ASSET_PARENT_TABLE_NAME = "event_asset_parent";
+    const EVENT_ASSET_INFO_TABLE_NAME = "asset_infos";
     
-    private $statement = NULL;
+    private $statement = array();
     
     public function __construct() {
         parent::__construct();
@@ -21,18 +22,34 @@ class ServerLogger extends Logger {
         }
         
         
-        $this->statement = $db_object->prepare(
+        $this->statement['insert_log'] = $db_object->prepare(
           'INSERT INTO '. db_gettable(ServerLogger::EVENT_TABLE_NAME) . ' (`asset`, `origin`, `asset_classroom_id`, `asset_course`, ' .
             '`asset_author`, `asset_cam_slide`, `event_time`, `type_id`, `context`, `loglevel`, `message`) VALUES (' .
-          ':asset, :origin, :classroom, :course, :author, :cam_slide, NOW(), :type_id, :context, :loglevel, :message)');
+          ':asset, :origin, :classroom, :course, :author, :cam_slide, :event_time, :type_id, :context, :loglevel, :message)');
         
-        if($this->statement == false) {
-            echo __CLASS__ . ": Prepared statement failed";
-            print_r($this->db->errorInfo());
-            throw new Exception("Prepared statement failed");
+        $this->statement['insert_asset_info'] = $db_object->prepare(
+            'INSERT INTO ' . db_gettable(ServerLogger::EVENT_ASSET_INFO_TABLE_NAME) . ' (asset, ' .
+                'start_time, asset_classroom_id, asset_course, asset_author) VALUES(' .
+                ':asset, :start_time, :asset_classroom_id, :asset_course, :asset_author)'
+            );
+        
+        $this->statement['update_asset_info'] = $db_object->prepare(
+            'UPDATE ' . db_gettable(ServerLogger::EVENT_ASSET_INFO_TABLE_NAME) . ' '
+                . 'SET end_time = :end_time '
+                . 'WHERE asset = :asset'
+            );
+        
+        
+        foreach ($this->statement as $state) {
+            if($state == false) {
+                echo __CLASS__ . ": Prepared statement failed";
+                print_r($this->db->errorInfo());
+                throw new Exception("Prepared statement failed");
+            }
         }
         
     }
+    
     
     /**
      * Logs with an arbitrary level.
@@ -46,38 +63,63 @@ class ServerLogger extends Logger {
      * @param AssetLogInfo $asset_info Additional information about asset if any, in the form of a AssetLogInfo structure
      * @return LogData temporary data, used by children functions
      */
-    public function log($type, $level, $message, array $context = array(), $asset = "dummy", $asset_info = null)
+    public function log($type, $level, $message, array $context = array(), $asset = "dummy", 
+            $author = null, $cam_slide = null, $course = null, $classroom = null)
     {
-        $tempLogData = parent::log($type, $level, $message, $context, $asset, $asset_info);
-        
+        $tempLogData = parent::log($type, $level, $message, $context, $asset, $author, $cam_slide, $course, $classroom);
         
         global $appname; // to be used as origin
         
-        insert_log($tempLogData->type_id, $tempLogData->log_level_integer, 
+        $this->insert_log($tempLogData->type_id, $tempLogData->log_level_integer, 
                 $message, $tempLogData->context, $asset, $appname, 
-                $asset_info->classroom, $asset_info->course, $asset_info->author, 
-                $asset_info->cam_slide);
-        
+                $classroom, $course, $author, 
+                $cam_slide, date('Y-m-d H:i:s'));
         
         return $tempLogData;
     }
     
     public function insert_log($type, $level, $message, $context, $asset, $origin, 
-            $classroom, $course, $author, $cam_slide) {
+            $classroom, $course, $author, $cam_slide, $event_time) {
         
-        $this->statement->bindParam(':asset', $asset);
-        $this->statement->bindParam(':origin', $origin);
-        $this->statement->bindParam(':classroom', $classroom);
-        $this->statement->bindParam(':course', $course);
-        $this->statement->bindParam(':author', $author);
-        $this->statement->bindParam(':cam_slide', $cam_slide);
-        $this->statement->bindParam(':type_id', $type);        
-        $this->statement->bindParam(':context', $context);
-        $this->statement->bindParam(':loglevel', $level);
-        $this->statement->bindParam(':message', $message);
+        $type_name = $this->get_type_name($type);
         
-        $this->statement->execute();
+        if($type_name== EventType::ASSET_CREATED) {
+            $this->insert_asset_infos($asset, $event_time, $classroom, $course, $author);
+            
+        } else if ($type_name == EventType::ASSET_RECORD_END) {
+            $this->insert_asset_infos_end($asset, $event_time);
+            
+        }
+        
+        $this->statement['insert_log']->bindParam(':asset', $asset);
+        $this->statement['insert_log']->bindParam(':origin', $origin);
+        $this->statement['insert_log']->bindParam(':classroom', $classroom);
+        $this->statement['insert_log']->bindParam(':course', $course);
+        $this->statement['insert_log']->bindParam(':author', $author);
+        $this->statement['insert_log']->bindParam(':cam_slide', $cam_slide);
+        $this->statement['insert_log']->bindParam(':type_id', $type);        
+        $this->statement['insert_log']->bindParam(':context', $context);
+        $this->statement['insert_log']->bindParam(':loglevel', $level);
+        $this->statement['insert_log']->bindParam(':message', $message);
+        $this->statement['insert_log']->bindParam(':event_time', $event_time);
+        
+        $this->statement['insert_log']->execute();
     }
     
+    public function insert_asset_infos($asset, $start_date, $classroom, $course, $author) {
+        $this->statement['insert_asset_info']->bindParam(':asset', $asset);
+        $this->statement['insert_asset_info']->bindParam(':start_time', $start_date);
+        $this->statement['insert_asset_info']->bindParam(':asset_classroom_id', $classroom);
+        $this->statement['insert_asset_info']->bindParam(':asset_course', $course);
+        $this->statement['insert_asset_info']->bindParam(':asset_author', $author);
+        
+        $this->statement['insert_asset_info']->execute();
+    }
     
+    public function insert_asset_infos_end($asset, $end_date) {
+        $this->statement['update_asset_info']->bindParam(':end_time', $end_date);
+        $this->statement['update_asset_info']->bindParam(':asset', $asset);
+        
+        $this->statement['update_asset_info']->execute();
+    }
 }

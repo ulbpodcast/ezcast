7341fb5ca9b8507471373ffc62802fcb7e81f563
arwillame <arnaud.willame@uclouvain.be>
2017-05-19 11:00:07 +0200

    Bug_fix: couldn't find renderder if !=0
    take the real list of renderer to find one

--- ezmanager/cli_update_title.php
+++ ezmanager/cli_update_title.php
@@ -22,7 +22,10 @@ $asset_metadata = metadata2assoc_array($album_path .'/'.$asset. "/_metadata.xml"
 if(!isset($album_metadata['course_code_public']))$album_metadata['course_code_public']='';
 
 // Get ip/name of renderer
-$ezrenderer_list= require $basedir.'/ezmanager/renderers.inc';		
+$ezrenderer_list= require $basedir.'/ezmanager/renderers.inc';	
+$keys=array_keys($ezrenderer_list);
+$ezrenderer=$ezrenderer_list[$keys[0]];
+
 $log_path=$repository_basedir.'/repository/'.$album.'/'.$asset.'/renderer_update_title.log';
 
 // with copy, ther is already a process in background so may generate conflict with the regeneration of the title.
@@ -39,13 +42,13 @@ if(isset($asset_metadata['add_title']) && $asset_metadata['status']=='processed'
 	// put the status of the asset to "processing"
 	$cmd1='sed -i "s/<status>processed<\/status>/<status>processing<\/status>/g" '.$repository_basedir.'/repository/'.$album.'/'.$asset.'/_metadata.xml';
 	// Copy asset in ezrenderer
-	$cmd2='rsync -avhp '.$repository_basedir.'/repository/'.$album.'/'.$asset.'/  '.$ezrenderer_list[0]['client'].'@'.$ezrenderer_list[0]['host'].':'.$ezrenderer_list[0]['processed_dir'].'/../processing/'.$album.'_'.$asset;
+	$cmd2='rsync -avhp '.$repository_basedir.'/repository/'.$album.'/'.$asset.'/  '.$ezrenderer['client'].'@'.$ezrenderer['host'].':'.$ezrenderer['processed_dir'].'/../processing/'.$album.'_'.$asset;
 	// launch the rendering
-	$cmd3='ssh '.$ezrenderer_list[0]['client'].'@'.$ezrenderer_list[0]['host'].' "'.$php_cli_cmd.' '.$ezrenderer_list[0]['processed_dir'].'/../../bin/gen_new_intro.php '.$ezrenderer_list[0]['processed_dir'].'/../processing/'.$album.'_'.$asset.' \"'.base64_encode($album_metadata['name']).'\"  \''.base64_encode($copyright).'\'  \''.base64_encode($organization_name).'\'   \''.base64_encode($album_metadata['id']).'\'  \''.base64_encode(suffix_remove($album)).'\'  \''.base64_encode($album_metadata['course_code_public']).'\' "';
+	$cmd3='ssh '.$ezrenderer['client'].'@'.$ezrenderer['host'].' "'.$php_cli_cmd.' '.$ezrenderer['processed_dir'].'/../../bin/gen_new_intro.php '.$ezrenderer['processed_dir'].'/../processing/'.$album.'_'.$asset.' \"'.base64_encode($album_metadata['name']).'\"  \''.base64_encode($copyright).'\'  \''.base64_encode($organization_name).'\'   \''.base64_encode($album_metadata['id']).'\'  \''.base64_encode(suffix_remove($album)).'\'  \''.base64_encode($album_metadata['course_code_public']).'\' "';
 	// get new files with good title
-	$cmd4='rsync -avhp '.$ezrenderer_list[0]['client'].'@'.$ezrenderer_list[0]['host'].':'.$ezrenderer_list[0]['processed_dir'].'/../processing/'.$album.'_'.$asset.'/'.' '.$repository_basedir.'/repository/'.$album.'/'.$asset;
+	$cmd4='rsync -avhp '.$ezrenderer['client'].'@'.$ezrenderer['host'].':'.$ezrenderer['processed_dir'].'/../processing/'.$album.'_'.$asset.'/'.' '.$repository_basedir.'/repository/'.$album.'/'.$asset;
 	// delete proccessifile         
-	$cmd5='ssh '.$ezrenderer_list[0]['client'].'@'.$ezrenderer_list[0]['host'].' "rm -rf '.$ezrenderer_list[0]['processed_dir'].'/../processing/'.$album.'_'.$asset.'"';
+	$cmd5='ssh '.$ezrenderer['client'].'@'.$ezrenderer['host'].' "rm -rf '.$ezrenderer['processed_dir'].'/../processing/'.$album.'_'.$asset.'"';
 	// put status of the asset to processed 
 	$cmd6='sed -i "s/<status>processing<\/status>/<status>processed<\/status>/g" '.$repository_basedir.'/repository/'.$album.'/'.$asset.'/_metadata.xml ';
 	
@@ -63,7 +66,7 @@ if(isset($asset_metadata['add_title']) && $asset_metadata['status']=='processed'
 																																										   
 	// file_put_contents($log_path,'cmd1 :  '.$cmd1. PHP_EOL .'cmd2  : '.$cmd2. PHP_EOL .'cmd3  : '.$cmd3. PHP_EOL .'cmd4  : '.$cmd4. PHP_EOL .'cmd5  : '.$cmd5. PHP_EOL .'cmd6  : '.$cmd6. PHP_EOL .'output : '.$cmdoutput. PHP_EOL .'returnval : '.$returncode. PHP_EOL .'return : '.$return,FILE_APPEND);
 	// file_put_contents($log_path,'return : '.$assoc_metadata['name'],FILE_APPEND);
-	// file_put_contents($log_path,'return : '.$ezrenderer_list[0]['name'],FILE_APPEND);
+	// file_put_contents($log_path,'return : '.$ezrenderer['name'],FILE_APPEND);
 
 }
 

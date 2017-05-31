2a7df2bfa41082e396a3e30611a8754aa8fb630e
Jan-David <jvbinsbe@ulb.ac.be>
2017-05-31 20:13:03 +0200

    Changed ezadmin push changes order (so that recorders comes last and ezmanager handling first)

--- ezadmin/controller/push_changes.php
+++ ezadmin/controller/push_changes.php
@@ -2,22 +2,21 @@
 
 function index($param = array()) {
     //TODO: DO THIS FUNCTION
-    // Save admins into ezmanager & recorders
-    $res = push_admins_to_recorders_ezmanager();
-
-    if (!$res) {
-        echo '<div class="alert alert-warning">' . template_get_message('push_to_recorders_unsuccessful', get_lang()) . '</div>';
-    }
-
-    // Save users & courses into recorder
-    push_users_courses_to_recorder();
-
     // Save additional users into ezmanager
     push_users_to_ezmanager();
 
     // Save changes to classroom into ezmanager
     push_classrooms_to_ezmanager();
 
+    // Save users & courses into recorder
+    push_users_courses_to_recorder();
+
+    // Save admins into ezmanager & recorders
+    $res = push_admins_to_recorders_ezmanager();
+
+    if (!$res) {
+        echo '<div class="alert alert-warning">' . template_get_message('push_to_recorders_unsuccessful', get_lang()) . '</div>';
+    }
     // Remove "save changes" alert
     notify_changes(false);
 

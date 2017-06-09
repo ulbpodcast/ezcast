086afe48e9f70faec66d9289dab591dcc585fc02
arwillame <arnaud.willame@uclouvain.be>
2017-06-09 12:47:02 +0200

    Bug fix: bookmark copy added

--- ezmanager/controller/asset_copy.php
+++ ezmanager/controller/asset_copy.php
@@ -15,19 +15,19 @@ function index($param = array()) {
     // Sanity checks
     //
     if (!isset($input['asset']) || !isset($input['from']) || !isset($input['to'])) {
-        echo 'Usage: index.php?action=move_asset&amp;from=SOURCE&amp;to=DESTINATION&amp;asset=ASSET';
+        echo 'Usage: index.php?action=copy_asset&amp;from=SOURCE&amp;to=DESTINATION&amp;asset=ASSET';
         die;
     }
 
     if (!acl_has_album_permissions($input['from']) || !acl_has_album_permissions($input['to'])) {
         error_print_message(template_get_message('Unauthorized', get_lang()));
-        log_append('warning', 'move_asset: you can\'t manage album ' . $input['from'] . ' or ' . $input['to']);
+        log_append('warning', 'copy_asset: you can\'t manage album ' . $input['from'] . ' or ' . $input['to']);
         die;
     }
 
     if (!ezmam_asset_exists($input['from'], $input['asset'])) {
         error_print_message(template_get_message('Non-existant_album', get_lang()));
-        log_append('warning', 'move_asset: asset ' . $input['asset'] . ' of album ' . $input['from'] . ' does not exist');
+        log_append('warning', 'copy_asset: asset ' . $input['asset'] . ' of album ' . $input['from'] . ' does not exist');
         die;
     }
     
@@ -38,6 +38,7 @@ function index($param = array()) {
     }
 
     // adds the previously saved bookmarks to the new album
+    $bookmarks = toc_asset_bookmark_list_get($input['from'], $input['asset']);
     $count = count($bookmarks);
     for ($index = 0; $index < $count; $index++) {
         $bookmarks[$index]['album'] = $input['to'];

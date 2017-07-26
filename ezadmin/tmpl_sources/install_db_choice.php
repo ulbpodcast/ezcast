daad569452e9d824bbc4e5103e597fa2967672f4
Rémy Detobel <detobel36@hotmail.fr>
2017-07-26 14:12:34 +0200

    Remove unused import (and fix link) + Add div to make a correct align

--- ezadmin/tmpl_sources/install_db_choice.php
+++ ezadmin/tmpl_sources/install_db_choice.php
@@ -5,15 +5,14 @@
         <title>®install_page_title®</title>
         <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet"/>
         <link href="css/style.css" rel="stylesheet"/>
-        <script type="text/javascript" src="./jquery-1.8.0.min.js"></script>
-        <script type="text/javascript" src="./modernizr.custom.23345.js"></script>
+        <script type="text/javascript" src="./js/jquery-2.2.4.min.js"></script>
 
     </head>
 
     <body link="#000088" vlink="#000044" alink="#0000ff" <?php if (isset($GLOBALS['debugmode']) && $GLOBALS['debugmode'] == "devl") echo 'background="#99ff99"' ?>>
         <div class="container_ezplayer">
             <?php include_once template_getpath("div_header.php"); ?>
-            <div id="global">
+            <div id="global" class="install">
             <h2 style="padding: 10px 0px;">®install_db_choice®</h2>
             <?php foreach ($errors as $e) {
                 ?>

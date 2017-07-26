4de549c9f0630b6bfbed82a667627ee71f6cb4ed
Rémy Detobel <detobel36@hotmail.fr>
2017-07-26 13:53:23 +0200

    Remove broken link to JS + remove unused code

--- ezadmin/tmpl_sources/install.php
+++ ezadmin/tmpl_sources/install.php
@@ -5,64 +5,8 @@
         <title>®install_page_title®</title>
         <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet"/>
         <link href="css/style.css" rel="stylesheet"/>
-        <script type="text/javascript" src="./jquery-1.8.0.min.js"></script>
-        <script type="text/javascript" src="./modernizr.custom.23345.js"></script>
+        <script type="text/javascript" src="./js/jquery-2.2.4.min.js"></script>
 
-        <script>
-            /**
-             * HTML5 Placeholder Text, jQuery Fallback with Modernizr
-             *
-             * @url        http://uniquemethod.com/
-             * @author    Unique Method
-             */
-            $(function()
-            {
-                // check placeholder browser support
-                if (!Modernizr.input.placeholder)
-                {
-
-                    // set placeholder values
-                    $(this).find('[placeholder]').each(function()
-                    {
-                        if ($(this).val() == '') // if field is empty
-                        {
-                            $(this).val($(this).attr('placeholder'));
-                        }
-                    });
-
-                    // focus and blur of placeholders
-                    $('[placeholder]').focus(function()
-                    {
-                        if ($(this).val() == $(this).attr('placeholder'))
-                        {
-                            $(this).val('');
-                            $(this).removeClass('placeholder');
-                        }
-                    }).blur(function()
-                    {
-                        if ($(this).val() == '' || $(this).val() == $(this).attr('placeholder'))
-                        {
-                            $(this).val($(this).attr('placeholder'));
-                            $(this).addClass('placeholder');
-                        }
-                    });
-
-                    // remove placeholders on submit
-                    $('[placeholder]').closest('form').submit(function()
-                    {
-                        $(this).find('[placeholder]').each(function()
-                        {
-                            if ($(this).val() == $(this).attr('placeholder'))
-                            {
-                                $(this).val('');
-                            }
-                        })
-                    });
-
-                }
-            });
-        </script>
-        <script src="bootstrap/js/bootstrap-dropdown.js"></script>
     </head>
 
     <body link="#000088" vlink="#000044" alink="#0000ff">

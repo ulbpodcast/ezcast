56e413c58d6a6e3dcd3013b7fd6c649b1489c8d3
Jan-David <jvbinsbe@ulb.ac.be>
2017-08-17 10:41:54 +0200

    Enable responsible design in all cases (instead of having an option to do it)

--- ezplayer/tmpl_sources/help.php
+++ ezplayer/tmpl_sources/help.php
@@ -35,12 +35,7 @@
         <link rel="shortcut icon" type="image/ico" href="images/Generale/favicon.ico" />
         <link rel="apple-touch-icon" href="images/ipadIcon.png" /> 
         <link rel="stylesheet" type="text/css" href="css/ezplayer_style_v2.css" />
-		<?php
-			global $enable_css_phone;
-			if ($enable_css_phone) {
-		?>
-			 <link rel="stylesheet" type="text/css" href="css/smartphone.css" />
-		<?php }  ?>
+        <link rel="stylesheet" type="text/css" href="css/smartphone.css" />
 		
         <script type="text/javascript" src="js/jQuery/jquery-2.1.3.min.js"></script>
         <script>

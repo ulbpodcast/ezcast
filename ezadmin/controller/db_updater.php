cd53c8f1b840e3187c448b37013670f27e723db1
Jan-David <jvbinsbe@ulb.ac.be>
2017-08-29 14:54:18 +0200

    Preparing for db updater page

---
+++ ezadmin/controller/db_updater.php
@@ -0,0 +1,12 @@
+<?php
+
+function index($param = array()) {
+    global $input;
+
+    if (isset($input['update'])) {
+        include template_getpath('div_db_updater_home.php');
+    } else {
+        include template_getpath('div_db_updater_updating.php'); //NYI
+    }
+}
+

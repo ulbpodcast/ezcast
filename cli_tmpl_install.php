5a98bc34945bc2cb7a4e90dbd70c8bc1094a6dc1
arwillame <arnaud.willame@uclouvain.be>
2017-09-01 12:56:50 +0200

    Centralise template generation in root directory

---
+++ cli_tmpl_install.php
@@ -0,0 +1,33 @@
+<?php
+require_once './commons/config.inc';
+
+$G="\033[32m\033[1m";
+$R="\033[31m\033[1m";
+$N="\033[0m";
+
+$components = array("ezmanager","ezplayer","ezadmin");
+$languages = array("fr","en");
+
+foreach ($components as $component) {
+    $source_folder = "./$component/tmpl_sources";
+    $dest_folder = "./$component/tmpl";
+    
+    if (!is_dir($dest_folder)) {
+        $res = mkdir ($dest_folder);
+        if (!$res) {
+            echo $R."ERROR : Could not create $dest_folder ! $N". PHP_EOL ;
+            continue;            
+        }
+    }
+    
+    echo "Compiling files ..." . PHP_EOL;
+    foreach ($languages as $language) {
+        exec(" $php_cli_cmd ./commons/cli_template_generate.php $source_folder $language $dest_folder ./$component/translations.xml");        
+    }    
+    echo $G."Compilation of $component complete. $N " . PHP_EOL;
+}
+
+
+
+
+

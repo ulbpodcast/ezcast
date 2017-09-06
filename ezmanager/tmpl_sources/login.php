e4b2ffd4562ba7d7524883455662fc04ce60805a
arwillame <arnaud.willame@uclouvain.be>
2017-09-06 15:30:52 +0200

    EZmanager/EZplayer: fix LoginForm in case of error. wrong bloc position

--- ezmanager/tmpl_sources/login.php
+++ ezmanager/tmpl_sources/login.php
@@ -4,20 +4,12 @@
         <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
         <title>®podman_page_title®</title>
         <?php include_once template_getpath('head_css_js.php'); ?>
-
     </head>
     <body>
         <div class="container">
             <?php include 'div_main_header.php'; ?>
             <div id="global">
-                <p>
-                    <br />
-                    <?php if(isset($error) && $error != "") { 
-                    echo '<div class="alert alert-danger col-md-4 col-md-offset-4" role="alert">';
-                        echo $error;
-                    echo '</div><br />';
-                    } ?>
-                    
+                <p> 
                      <?php if($sso_enabled) { ?>
                     <div class="login-choices btn-center" >
                           <a class="btn-login btn-sso" href="<?php global $ezplayer_safe_url; echo $ezplayer_safe_url;
@@ -25,6 +17,14 @@
                           <a class="btn-login btn-login-default btn-default" href="#" id="default_auth_button" title="">®authLocal®</a>
                     </div>
                     <?php } ?>
+                    
+                    <br />
+                    
+                    <?php if(isset($error) && $error != "") { 
+                        echo '<div class="alert alert-danger col-md-4 col-md-offset-4" role="alert">';
+                        echo $error;
+                        echo '</div><br />';
+                    } ?>
                     
                     <form id="form_login" class="form_login form-horizontal col-md-6 col-md-offset-3" method="post" action="<?php
                           global $ezmanager_safe_url;
@@ -70,14 +70,12 @@
                         </div>
                 </form>
                 </p>
-
             </div>
-
             <?php include 'div_main_footer.php'; ?>
         </div>
         
-        
         <script type="text/javascript">
+            
             function detect_flash() {
                 if ((navigator.mimeTypes ["application/x-shockwave-flash"] == undefined)) {
                     document.form_login.has_flash.value = 'N';
@@ -89,6 +87,9 @@
             <?php if($sso_enabled) { ?>
                 $( document ).ready(function() {
                     $("#form_login").hide();
+                    <?php if(isset($error) && $error != "") { ?>
+                        $("#form_login").show();      
+                    <?php } ?>
                     $("#default_auth_button").click(function () {
                         $("#form_login").slideToggle('fast');
                         return false;

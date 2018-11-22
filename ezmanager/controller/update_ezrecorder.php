b5b9436e4d210e87c0b7e6bd76653a7150cbfb15
arwillame <arnaud.willame@uclouvain.be>
2018-11-22 16:53:47 +0100

    EZmanager: minor bug fix check password recorder

--- ezmanager/controller/update_ezrecorder.php
+++ ezmanager/controller/update_ezrecorder.php
@@ -23,7 +23,7 @@ foreach ($album as $album_name) {
 }
 
 $ezrecorder_pw=$input['ezrecorder_pw'];
-if($ezrecorder_pw!="" AND $ezrecorder_pw!="PASSWORD"){
+if($ezrecorder_pw!="" AND !empty($ezrecorder_pw)){
   //password changed
   if(!db_user_read($_SESSION['user_login'])){
     //user doesn't exist yet, lets create it

4a58bca67f2689163a65c0ea25a17738b6b41db1
Ayoub Chaalane <ayoub.chaalane@uclouvain.be>
2018-11-23 14:13:51 +0100

    EZadmin: add options (camera) to edit config

--- ezadmin/controller/edit_config.php
+++ ezadmin/controller/edit_config.php
@@ -9,8 +9,11 @@ function index($param = array())
         $add_users = (array_key_exists('add_users_enabled', $input) && $input['add_users_enabled'] == 'on');
         $pwd_storage = (array_key_exists('password_storage_enabled', $input) && $input['password_storage_enabled'] == 'on');
         $use_user_name = (array_key_exists('users_by_name', $input) && $input['users_by_name'] == 'on');
+        $control_panel = (array_key_exists('enable_control_panel', $input) && $input['enable_control_panel'] == 'on');
+        $control_panel_options = (array_key_exists('enable_control_panel_options', $input) && $input['enable_control_panel_options'] == 'on');
 
-        update_config_file($allow_recorder, $add_users, $pwd_storage, $use_user_name);
+
+        update_config_file($allow_recorder, $add_users, $pwd_storage, $use_user_name, $control_panel, $control_panel_options);
 
         $alert = '<div class="alert alert-success">' . template_get_message('save_successful', get_lang()) . '</div>';
     }

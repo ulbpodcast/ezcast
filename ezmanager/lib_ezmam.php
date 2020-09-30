8da8bab1e65c2087ab79a03b6fc3f42d80666d8b
arwillame <arnaud.willame@uclouvain.be>
2020-09-30 13:33:43 +0200

    remove reput

--- ezmanager/lib_ezmam.php
+++ ezmanager/lib_ezmam.php
@@ -876,8 +876,8 @@ function ezmam_asset_list_metadata($album)
                   }
                 }
                 $asset_metadata['order'] = $idx;
-                ezmam_asset_metadata_set($album, $asset, $asset_metadata);
-                sleep(2);
+                // ezmam_asset_metadata_set($album, $asset, $asset_metadata);
+
                 $asset_list[$idx]['name'] = $asset;
                 $asset_list[$idx]['metadata'] = $asset_metadata;
                 $idxUsed[] = $idx;
